2aec75f4ae1848c55caf89efca98f30bb4c82309
virgil-rouquettecampredon <virgil.rouquette-campredon@etu.umontpellier.fr>
2020-12-02 12:36:25 +0100

    EvalNegatif + EvalEntreDeuxOperandes Ainsi que AnalyserString fait ou corriger

--- src/js/Expression.js
+++ src/js/Expression.js
@@ -10,6 +10,15 @@ class Expression {
         let compteurTableau = 0;
         let debutOperande = debutString;
         for (let i = debutString; i < finString; i++) {
+            while (StringADonner[i] == "¬"){
+                if (this.estNegatif){
+                    this.estNegatif = false;
+                }
+                if (!this.estNegatif){
+                    this.estNegatif = true;
+                }
+                i++;
+            }
             if (StringADonner[i] == "("){
                 nbParanthese++;
             }
@@ -18,6 +27,9 @@ class Expression {
             }
             if (nbParanthese == 0){
                 let ope ="";
+                if (this.estNegatif){
+                    this.estNegatif = false;
+                }
                 for (let j = debutOperande; j <= i; j++) {
                     ope += StringADonner[j];
                 }
@@ -33,6 +45,88 @@ class Expression {
                 if (operande1[0] == "(" && operande1[operande1.length-1] == ")"){
                     this.AnalyserString(operande1,1, operande1.length-1);
                 }
+               if (operande1[0] == "¬" && operande1[1] == "(" && operande1[operande1.length-1] == ")"){
+                    this.evalNegation(operande1, 2, operande1.length-1);
+                }
+            }
+        }
+    }
+
+    evalEntreDeuxOperandes(operande1, operateur, operande2){
+        let n = 0;
+        switch (operateur){
+            case "∧":
+                operande1 = operande1;
+                operande2 = operande2;
+                n = 1;
+                break;
+            case "∨":
+                operande1 = operande1;
+                operande2 = operande2;
+                n = 2;
+                break;
+            case "→":
+                if (operande1[0] == "¬"){
+                    operande1 = operande1.substring(1);
+                    operande2 = operande2;
+                }
+                else{
+                    operande1 = "¬(" + operande1 + ")";
+                    operande2 = operande2;
+                }
+                operateur = "∨";
+                n = 2;
+                break;
+            case "↔":
+                operande1 = "(" + operande1 + "→" + operande2 + ")";
+                operande2 = "(" + operande2 + "→" + operande1 + ")";
+                operateur = "∧"
+                n = 1;
+                break;
+            default:
+                alert("Un des caractère n'est pas un utilisé en logique des propositions !");
+        }
+    }
+
+    evalNegation(StringADonner, debutString, finString) {
+        let doubleImpliqNega = false;
+        let caractere = "";
+        this.AnalyserString(StringADonner, debutString, finString);
+        for (let i = 0; i < this.tableauOperande.length; i++) {
+            let operande = this.tableauOperande[i];
+
+            if (operande[0] == "∧"){
+                this.tableauOperande[i] = "∨";
+            }
+            else if (operande[0] == "∨"){
+                this.tableauOperande[i] = "∧";
+            }
+            else if (operande[0] == "→"){
+                let operandePrecedente = this.tableauOperande[i-1];
+                if (operandePrecedente[0] == "¬"){
+                    this.tableauOperande[i-1] = this.tableauOperande[i-1].substring(1);
+                }
+                else {
+                    this.tableauOperande[i-1] = "¬" + this.tableauOperande[i-1];
+                }
+                this.tableauOperande[i] = "∧";
+
+            }
+            else if (operande[0] == "↔"){
+                this.tableauOperande[i] = "∨";
+                 caractere = this.tableauOperande[i-1].substring(1);
+                this.tableauOperande[i-1] = "¬(" + caractere + "→" + this.tableauOperande[i+1] + ")";
+                doubleImpliqNega = true;
+            }
+            else if (doubleImpliqNega){
+                this.tableauOperande[i] = "¬(" + this.tableauOperande[i] + "→" + caractere + ")";
+                doubleImpliqNega = false;
+            }
+            else if (operande[0] == "¬"){
+                this.tableauOperande[i] = this.tableauOperande[i].substring(1);
+            }
+            else {
+                this.tableauOperande[i] = "¬" + this.tableauOperande[i];
             }
         }
     }
