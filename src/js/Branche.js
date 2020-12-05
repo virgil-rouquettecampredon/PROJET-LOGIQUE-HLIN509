916d937d1f46f190a751b76e9c31f0a439f9b820
erwan-reinders <erwan.reinders@etu.umontpellier.fr>
2020-12-05 17:14:12 +0100

    gestion canvas pas finie

--- src/js/Branche.js
+++ src/js/Branche.js
@@ -29,34 +29,51 @@ class Branche {
 
             //On va ensuite récupérer la distance entre la branche pere par rapport au container du père
             let pere_distance_haut = pere_branche.offsetTop + pere_wrapper.offsetTop;
+
+            console.log(pere_distance_haut);
+
             //Distance entre bas branche pere et container pere
-            pere_distance_haut = pere_distance_haut - pere_branche.offsetHeight;
+            pere_distance_haut = pere_distance_haut + pere_branche.offsetHeight;
             //Distance entre pere et container pere par la droite
             let pere_distance_droite = pere_branche.offsetLeft + pere_wrapper.offsetLeft + pere_branche.offsetWidth/2;
 
-
-            //Pour cela, On cré un canvas 2D sur lequel on va générer un trait
+            //Pour cela, On créer un canvas 2D sur lequel on va générer un trait
             let canvas2D = document.createElement("canvas");
             //On va donner comme classe le tag de dessin
             canvas2D.classList.add(TAGS.TAG_dessinLiaison);
             //Et une taille en px équivalente à celle du container de this
-            canvas2D.style.width = this.container.offsetWidth + "px";
+            canvas2D.style.width = "100%";
+
+            console.log(this);
+            console.log("Largeur canv");
+            console.log(this.container.offsetWidth);
+
             //Ce canvas sera en position absolute par rapport au container de this, soit placé sous la branche pere
             canvas2D.style.top = pere_distance_haut + "px";
-            canvas2D.style.left = "0px";
-            let ctxDessin = canvas2D.getContext("2d");
-
+            canvas2D.style.left = "0";
+            canvas2D.style.right = "0";
             let hauteur_canvas2D = 0;
 
+            //Tableau des liaisons a dessiner
+            let traits = [];
+            /*let intDeb = 1;
+            while (intDeb<=this.fils.length){
+                let curXB = 1000*intDeb/(this.fils.length*2);
+                traits.push([[500,0],[curXB,500]]);
+                intDeb = intDeb +2;
+            }*/
             //Pour générer un trait, il faut avant tout récupérer les coordonnées de ses deux points extrémités
             //Ainsi, pour chaque fils présents, on va récupérer les coordonnées du milieu-haut de  l'emplacement de leur branche
             for (let i = 0; i < this.fils.length; i++) {
                 //On récupère son élément HTML (sa branche)
                 let fils_branche = this.fils[i].contenu;
+
                 //Cette branche est contnu dans un wrapper
                 let fils_wrapper = fils_branche.closest("."+ TAGS.TAG_Wrapper);
+
                 //wrapper lui meme contenu dans un container
                 let fils_container = this.fils[i].container;
+
                 //On va ensuite récupérer la distance entre la branche fils par rapport au container du père
                 let fils_distance_haut = fils_branche.offsetTop + fils_wrapper.offsetTop + fils_container.offsetTop;
 
@@ -69,17 +86,41 @@ class Branche {
                 if(fils_distance_haut-pere_distance_haut>hauteur_canvas2D){
                     hauteur_canvas2D = fils_distance_haut-pere_distance_haut;
                 }
-                //On peut donc dessiner
-                ctxDessin.beginPath();
-                //0 car le canvas démare en bas de la branche pere, soit à une distance y de 0
-                ctxDessin.moveTo(pere_distance_droite,0);
-                ctxDessin.lineTo(fils_distance_droite,hauteur_canvas2D);
-                ctxDessin.stroke();
-                ctxDessin.closePath();
+                //Valeurs des deux points extrèmités du trait à tracer
+                traits.push([[pere_distance_droite,0],[fils_distance_droite,hauteur_canvas2D]]);
             }
             canvas2D.style.height = hauteur_canvas2D + "px";
+            console.log("Hauteur canv");
+            console.log(hauteur_canvas2D);
+
             //On va placer cle canvas dans le container du pere des enfants, donc this
-            this.container.append(canvas2D);
+            this.container.appendChild(canvas2D);
+
+            //On récupère ensuite le canvas créé
+            let canv = this.container.getElementsByClassName(TAGS.TAG_dessinLiaison)[0];
+            console.log(canv);
+            let ctxDessin = canv.getContext("2d");
+            console.log(ctxDessin);
+            console.log(traits);
+            let rapport = canv.style.width/canv.style.height;
+            if(rapport<1){
+                rapport = canv.style.height/canv.style.width;
+            }
+            //On peut donc dessiner
+            //On va dessiner tous les traits
+            for (let i = 0; i < traits.length; i++) {
+                ctxDessin.beginPath();
+                ctxDessin.lineWidth = 5;
+                ctxDessin.strokeStyle = "black";
+                ctxDessin.moveTo(traits[i][0][1], traits[i][0][0]);
+                ctxDessin.lineTo(traits[i][1][1], traits[i][1][0]*rapport);
+                ctxDessin.stroke();
+                console.log("DESSIN TRAIT");
+                console.log("X : "+ traits[i][0][0]);
+                console.log("Y : "+ traits[i][0][1]);
+                console.log("X : "+ traits[i][1][0]);
+                console.log("Y : "+ traits[i][1][1]);
+            }
         }else{
             console.log(this);
             console.log("Cette branche n'a pas de fils !");
